c5ae08b46c563bf8d84006aee3d735ecec0e6fcc
Oicanji <51388779+Oicanji@users.noreply.github.com>
2022-04-11 21:40:32 -0300

    Update aula-2-2.py

--- Estrutura-de-Dados/aula-2-2.py
+++ Estrutura-de-Dados/aula-2-2.py
@@ -1,13 +1,13 @@
-from cProfile import label
+
 import numpy as np 
 import matplotlib.pyplot as plt
 
 n = np.linspace(1, 10, 100)
-labels = ['Constante', 'Linear', 'Quadratica', 'Cubica']
-big_o = [np.ones(n.shape), np.log(n), n]
+labels = ['Constante', 'Logaritmo', 'linear', 'loglinear', 'Quadratica', 'Cubica', 'Exponencial']
+big_o = [np.ones(n.shape), np.log(n), n, np.multiply(n, np.log(n)), np.multiply(n, n), np.multiply(np.multiply(n, n), n), np.exp2(n)]
 
 plt.figure(figsize=(5, 4))
-plt.ylim(0, 10)
+plt.ylim(0, 100)
 
 for i in range(len(big_o)):
     plt.plot(n, big_o[i], label=labels[i])
